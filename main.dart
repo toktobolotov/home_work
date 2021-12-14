a4ef86ffd3b365566970a8601b3b3367e189f941
toktobolotov <as1a77@yandex.ru>
2021-12-14 14:43:18 +0300

    'first'

--- main.dart
+++ main.dart
@@ -1,19 +1,3 @@
 import 'dart:io';
 
-void main() {
-  print('введите порядковый номер пальца:');
-
-  var a = int.parse(stdin.readLineSync()!);
-
-  if (a == 1) {
-    print('Большой палец');
-  } else if (a == 2) {
-    print('Указательный палец');
-  } else if (a == 3) {
-    print('Средний палец');
-  } else if (a == 4) {
-    print('Безымяный палец');
-  } else if (a == 5) {
-    print('Мизинец');
-  }
-}
+void main() {}
