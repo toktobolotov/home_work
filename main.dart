54e40b2b6933f724ca3e92de2b5d191da997a65e
toktobolotov <as1a77@yandex.ru>
2021-12-14 16:09:10 +0300

    задание 1

--- main.dart
+++ main.dart
@@ -1,3 +1,19 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  print('введите порядковый номер пальца:');
+
+  var a = int.parse(stdin.readLineSync()!);
+
+  if (a == 1) {
+    print('Большой палец');
+  } else if (a == 2) {
+    print('Указательный палец');
+  } else if (a == 3) {
+    print('Средний палец');
+  } else if (a == 4) {
+    print('Безымяный палец');
+  } else if (a == 5) {
+    print('Мизинец');
+  }
+}
