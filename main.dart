db2fa44803e870523e0622aae002b4c60927f0fb
toktobolotov <as1a77@yandex.ru>
2021-12-14 18:48:46 +0300

    пытался что то делать

--- main.dart
+++ main.dart
@@ -1,3 +1,11 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  List arr1 = [1, 2, 4];
+  List arr2 = [1, 3, 4];
+  List arr3 = [];
+  for (int i = 0; i < arr1.length; i++) {
+    arr3 = arr1[i] + arr2;
+    print(arr3);
+  }
+}
