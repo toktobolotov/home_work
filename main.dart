782d17fe92e9a46bb061e8009fc1b0f7d69ef059
toktobolotov <as1a77@yandex.ru>
2021-12-15 17:49:40 +0300

    while

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  {
+    int i = 10;
+    print('Dart do while loop example');
+
+    do {
+      print(i);
+      i++;
+    } while (i <= 20);
+    print('the loop is terminated');
+  }
+}
