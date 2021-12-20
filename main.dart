867853d8dd5c2e8f9782b50a09e0f715762ea7fe
toktobolotov <as1a77@yandex.ru>
2021-12-20 18:15:15 +0300

    start

--- main.dart
+++ main.dart
@@ -1,39 +1,10 @@
 import 'dart:io';
 
 void main() {
-//   var arr = [1, 2, 5];
-//   print(arr);
-// }
-
-// String(List arr) {
-//   for (int i = 0; i < arr.length; i++) {
-//     if (arr[i] == 5) {
-//       return 'yes';
-//     } else {
-//       return 'No';
-//     }
-//   }
-// }
-// -------------------------------------------------------------
-//   var x = chislo(7, 4);
-//   print(x);
-// }
-
-// String chislo(int a, int b) {
-//   if (a == b) {
-//     return 'yes';
-//   } else {
-//     return 'no';
-//   }
-// }
-  var x = chislo(1);
-  print(x);
+  addNums('2,5,1,8,4');
 }
 
-bool chislo(int a) {
-  if (a < 0) {
-    return true;
-  } else {
-    return false;
-  }
+void addNums(String nums) {
+  var b = nums.split(',');
+  print(b);
 }
