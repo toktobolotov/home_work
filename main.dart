6b8bdb20e85af6cf31450ed05dfa47a7160bd208
toktobolotov <as1a77@yandex.ru>
2021-12-23 11:12:12 +0300

    до делать

--- main.dart
+++ main.dart
@@ -1,10 +1,16 @@
-import 'dart:io';
+import 'dart:core';
 
 void main() {
   addNums('2,5,1,8,4');
+  // addNums('10');
+  // addNums('1,2,3,4,5,6,7');
 }
 
 void addNums(String nums) {
   var b = nums.split(',');
-  print(b);
+  for (int i = 0; i < b.length; i++) {
+    var c = [];
+    c += b[i];
+    print(c);
+  }
 }
