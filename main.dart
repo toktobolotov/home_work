207619d67d8a8583c678ec0cc42fe3128ea3e18a
toktobolotov <as1a77@yandex.ru>
2022-01-05 09:15:47 +0300

    123

--- main.dart
+++ main.dart
@@ -1,16 +1,59 @@
-import 'dart:core';
-
 void main() {
-  addNums('2,5,1,8,4');
-  // addNums('10');
-  // addNums('1,2,3,4,5,6,7');
+//   chislo(null, null);
+// }
+
+// void chislo(int? a, int? b) {
+//   a ?? 1;
+//   b ?? 2;
+//   var c = a + b!;
+//   print(c);
+// }
+// --------------------------------------------------------------
+  word(null);
 }
 
-void addNums(String nums) {
-  var b = nums.split(',');
-  for (int i = 0; i < b.length; i++) {
-    var c = [];
-    c += b[i];
-    print(c);
-  }
+void word(String? name) {
+  print(name ?? 'error');
 }
+// ------------------------------------------------
+//   arr([
+//     [1, 2, 4],
+//     [4, 5],
+//     [6, 7]
+//   ]);
+// }
+
+// void arr(List<List<int>> a) {
+//   var b = [];
+//   for (int i = 0; i < a.length; i++) {
+//     b.addAll(a[i]);
+//   }
+//   print(b);
+// }
+// ----------------------------------------------
+//   spelling('bee');
+// }
+
+// void spelling(String word) {
+//   var arr = [];
+//   for (int i = 0; i < word.length; i++) {
+//     arr.add(word.substring(0, i + 1));
+//   }
+//   print(arr);
+// }
+// --------------------------------------------------------------------------
+//   addNums('2,5,1,8,4');
+//   // addNums('10');
+//   // addNums('1,2,3,4,5,6,7');
+// }
+
+// void addNums(String nums) {
+//   var b = nums.split(',');
+//   int c = 0;
+//   for (int i = 0; i < b.length; i++) {
+//     c += int.parse(b[i]);
+//   }
+//   print(c);
+// }
+// --------------------------------------------------------
+
