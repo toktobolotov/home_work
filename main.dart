5aea37d0026a53c6c16b8dd84c9f7e6ab5a831db
toktobolotov <as1a77@yandex.ru>
2022-01-05 13:26:43 +0300

    guess number of program by user

--- main.dart
+++ main.dart
@@ -1,32 +1,51 @@
 import 'dart:io';
-
 import 'dart:math';
 
 void main() {
-  print('загадайте число от 1 до 100');
-  var myNumb = int.parse(stdin.readLineSync()!);
+  print('Угадай число, загаданное программой!');
   var rng = Random();
-  var start = 0, end = 100;
-  var guess = (start + end) ~/ 2;
-  var tries = 1;
-  if (myNumb == guess) {
-    print('Вау, с первой попытки');
-  } else {
-    while (guess != myNumb) {
-      if (myNumb > guess) {
-        print('more than: $guess');
-        guess = rng.nextInt(max(guess, end));
-        tries += 1;
-      } else if (myNumb < guess) {
-        print('less than: $guess');
-        guess = rng.nextInt(max(start, guess));
-        tries += 1;
-      }
+  var progrNumb = rng.nextInt(100);
+  var check;
+  var popytka = 0;
+  while (progrNumb != check) {
+    var check = int.parse(stdin.readLineSync()!);
+    if (progrNumb > check) {
+      print('больше');
+      popytka += 1;
+    } else if (progrNumb < check) {
+      print('меньше');
+      popytka += 1;
+    } else {
+      print('Поздравляю ты угадал число $progrNumb с $popytka-го раза');
     }
-    print(myNumb);
-    print('from $tries tries guessed');
   }
 }
+
+  // -----------------------------------------------
+//   print('загадайте число от 1 до 100');
+//   var myNumb = int.parse(stdin.readLineSync()!);
+//   var rng = Random();
+//   var start = 0, end = 100;
+//   var guess = (start + end) ~/ 2;
+//   var tries = 1;
+//   if (myNumb == guess) {
+//     print('Вау, с первой попытки');
+//   } else {
+//     while (guess != myNumb) {
+//       if (myNumb > guess) {
+//         print('more than: $guess');
+//         guess = rng.nextInt(max(guess, end));
+//         tries += 1;
+//       } else if (myNumb < guess) {
+//         print('less than: $guess');
+//         guess = rng.nextInt(max(start, guess));
+//         tries += 1;
+//       }
+//     }
+//     print(myNumb);
+//     print('from $tries tries guessed');
+//   }
+// }
 
 //   var rng = new Random();
 
