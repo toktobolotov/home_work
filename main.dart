186c438b8a4254a248e8493ffa156ee36b1ace01
toktobolotov <as1a77@yandex.ru>
2022-01-07 17:57:30 +0300

    nasledovanie

--- main.dart
+++ main.dart
@@ -1,15 +1,17 @@
-import 'dart:io';
+class Bird {
+  void fly() {
+    print('The bird can fly');
+  }
+}
+
+class Parrot extends Bird {
+  void speak() {
+    print('The parrot can speak');
+  }
+}
 
 void main() {
-  List arr = List.generate(99, (index) => index);
-  List arr2 = [];
-
-  int i = 0;
-  while (i < arr.length) {
-    if (arr[i].isEven) {
-      arr2.add(arr[i]);
-    }
-    ++i;
-  }
-  print(arr2);
+  Parrot p = new Parrot();
+  p.speak();
+  p.fly();
 }
