ed976d0722ae6241648114580342f0a8d6f4e5fd
toktobolotov <as1a77@yandex.ru>
2022-01-10 09:24:56 +0300

    nasled

--- main.dart
+++ main.dart
@@ -1,12 +1,18 @@
-class Bird {
+class Fly_Animals {
   void fly() {
     print('The bird can fly');
   }
 }
 
-class Parrot extends Bird {
+class Parrot extends Fly_Animals {
   void speak() {
     print('The parrot can speak');
+  }
+}
+
+class Eagle extends Fly_Animals {
+  void vision() {
+    print('The eagle has a sharp vision');
   }
 }
 
