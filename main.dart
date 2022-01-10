44a87cba8aa2afdd5af58c5b6dfe378e924ce250
toktobolotov <as1a77@yandex.ru>
2022-01-10 17:30:40 +0300

    as

--- main.dart
+++ main.dart
@@ -1,15 +1,39 @@
-import 'dart:io';
+class Worker {
+  String? name;
+  String? surname;
+  int? rate;
+  int? days;
+
+  Worker({this.name, this.surname, this.days, this.rate});
+
+  num getSalary() {
+    final salary = (rate! * days!);
+    return salary;
+  }
+}
+
+class MyString {
+  String? somewords;
+
+  MyString({this.somewords});
+
+   String reverseMethod() {
+    final input = somewords.split('').reversed.join();
+    return input;
+   
+}
 
 void main() {
-  List arr = List.generate(99, (index) => index);
-  List arr2 = [];
+  Worker director =
+      Worker(name: 'Eldiyar', surname: 'Sultankulov', rate: 5000, days: 21);
 
-  int i = 0;
-  while (i < arr.length) {
-    if (arr[i].isEven) {
-      arr2.add(arr[i]);
-    }
-    ++i;
-  }
-  print(arr2);
+  Worker driver =
+      Worker(name: 'Rashid', surname: 'Bolotov', rate: 1000, days: 28);
+
+  print(director.getSalary());
+  print(driver.getSalary());
+
+  MyString education = MyString(somewords: 'education');
+
+  print(education.reverseMethod());
 }
